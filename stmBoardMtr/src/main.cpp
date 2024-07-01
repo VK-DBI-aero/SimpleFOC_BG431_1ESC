5acd202f380effebd42455dd86945432f915bcc6
Vincent Keiper <vince.keiper@dbiaero.com>
2024-07-01 12:59:10 -0400

    Attemped lowside current sense, unsure if correct

--- stmBoardMtr/src/main.cpp
+++ stmBoardMtr/src/main.cpp
@@ -14,25 +14,36 @@ LowsideCurrentSense current_sense = LowsideCurrentSense(0.003f, -64.0/7.0, A_OP1
 
 
 void setup() {
+
+  //DRIVER INITIALIZATION
   // pwm frequency to be used [Hz]
   driver.pwm_frequency = 30000;
   // power supply voltage [V]
   driver.voltage_power_supply = 12;
   // Max DC voltage allowed - default voltage_power_supply
   driver.voltage_limit = 12;
-
   // driver init
   driver.init();
-  // enable driver
-  driver.enable();
 
+  //MOTOR INITIALIZATION
+  //Link the motor to the Driver and Current Sensor
   motor.linkDriver(&driver);
+  motor.linkCurrentSense(&current_sense);
+  //set motor control type
+  motor.controller = MotionControlType::velocity;
 
-  // link the driver with the current sense
+  //setup serial monitoring
+  Serial.begin(115200);
+  motor.useMonitoring(Serial);
+
+  motor.init();
+  // Will eventually go here
+  //motor.initFOC();
+
+  //CURRENT SENSOR INITIALIZATION
   current_sense.linkDriver(&driver);
-  // init current sense
   current_sense.init();
-
+  motor.linkCurrentSense(&current_sense);
 
   _delay(1000);
 }
@@ -41,6 +52,17 @@ void loop() {
     // setting pwm (A: 3V, B: 1V, C: 5V)
     driver.setPwm(3,1,5);
     delay(8);
+    Serial.print("Current ==");
+    PhaseCurrent_s currents = current_sense.getPhaseCurrents();
+    float current_mag = current_sense.getDCCurrent();
+    Serial.print(currents.a*1000); // milli Amps
+    Serial.print("\t");
+    Serial.print(currents.b*1000); // milli Amps
+    Serial.print("\t");
+    Serial.print(currents.c*1000); // milli Amps
+    Serial.print("\t");
+    Serial.println(current_mag*1000); // milli Amps
+    Serial.println();
     driver.setPwm(5,3,1);
     delay(8);
     driver.setPwm(1,5,3);
