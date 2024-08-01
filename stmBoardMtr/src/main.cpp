762e4218a7442863e7c117dd5ae8a020c57a0109
Vincent Keiper <vince.keiper@dbiaero.com>
2024-08-01 09:50:14 -0400

    consistency commit

--- stmBoardMtr/src/main.cpp
+++ stmBoardMtr/src/main.cpp
@@ -3,161 +3,184 @@
 #include <string>
 #include "stm32g4xx_hal.h"
 
+//Operation Mode Definitions
 #define INCREMENT true
 #define DECREMENT false
 
-//local variable definitions
-int 	spinIt = 1;         // iterator for the startup
-int 	spinItSpeed = 500; // rate of the startup, higher = slower
-int 	mxRads = 0;         // max motor speed
-int 	outIt = 0;          //used for communication rate iteration
-float 	radPerSec = 0;    // driver the motor speed
-float 	target = 0.0;     // used to change currently tested variable
-String 	received_chars = "";
-bool 	opMode = true;
-float	ddZn = 0.0;
+// BEMF for 20 volts @ 3282 RPM
+//  Vbemf = BEMF constant * angular velocity
+//  2.68 = K * 343.690236 rad/s
+//  Bemf Constant = .007797719339
+// local variable definitions
+int spinIt = 1;             // iterator for the startup
+int spinItSpeed = 200;      // rate of the startup, higher = slower
+int mxRads = 0;             // max motor speed, set but uart commands later
+float radPerSec = 0;   
+String received_chars = ""; // stores the uart recieved chars
+bool opMode = INCREMENT;    // operation mode, increment or decrement
+float ddZn = 0.0;           // used to change dead zone. TODO: Implement
+float numInt;               // converts the uart in to int 
+float numFlt;               // converts the uart in to float
 
-
-
-//Set up the motor and driver, motor is 7 pp, .12 phase resistor, and 1400KV 
+// Set up the motor and driver, motor is 7 pp, .12 phase resistor, and 1400KV
 BLDCMotor motor = BLDCMotor(7, .12, 1400);
+// Used pins defined by stmboard to be controlled by the driver
 BLDCDriver6PWM driver = BLDCDriver6PWM(A_PHASE_UH, A_PHASE_UL, A_PHASE_VH, A_PHASE_VL, A_PHASE_WH, A_PHASE_WL);
 
-LowsideCurrentSense currentsense  = LowsideCurrentSense(0.003, -64.0/7.0,A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
+// Set up the ablility to sense current TODO:Make work, this is right but later use does not
+LowsideCurrentSense currentsense = LowsideCurrentSense(0.003, -64.0 / 7.0, A_OP1_OUT, A_OP2_OUT, A_OP3_OUT);
 
-/*controls the motor using the UART. 
--g starts the motor and revs up to 300 rad/s, 
--s stops the motor, 
--and entering a number sets the P in pid to that number
-  -(currently not useful, but implemented for later tuning)
+
+
+/********************************UART COMMANDS*********************************/
+//for optimal speeds the UART commands use function pointers in a LUT that is selected by the first character of the uart command 
+
+typedef void (*FunctionPointer)();
+
+//stops the motor by setting the current speed and max speed to 0
+void stopMtr()
+{
+  radPerSec = 0;
+  mxRads = 0;
+  received_chars = "";
+  Serial.println("Stopping Motor.");
+}
+
+//start the motor by setting the max speed to a default of 300 rad/s and current speed to 0
+void startMtr()
+{
+  radPerSec = 0;
+  mxRads = 300;
+  received_chars = "";
+  Serial.println("Starting Motor.");
+}
+
+//will be used to adjust the deadzone for fine tuning
+void ddZnSet()
+{
+  driver.dead_zone = numFlt;
+  radPerSec = 0;
+  received_chars = "";
+  Serial.print("Setting dead zone to ");
+  Serial.println(numFlt);
+}
+
+// Sets the motors speed to a user defined value recieved over UART
+// it first checks if the current value is larger of smaller than the current target and set the operation mode. The mxRads is then set to the target
+void setMtrTrgt()
+{
+  if (mxRads < numInt)
+  {
+    opMode = INCREMENT;
+  }
+  else
+  {
+    opMode = DECREMENT;
+  }
+
+  mxRads = numInt;
+  spinIt = 1;
+
+  Serial.print("Changing target speed to ");
+  Serial.print(numInt * 9.549297);
+  Serial.println("RPM.");
+  received_chars = "";
+}
+
+/* LUT that is used to select the function. 
+    0 = stop motor
+    1 = start motor
+    2 = set the dead zone
+    3 = set the motor speed
+
 */
-void serialControl(){
+FunctionPointer uartLUT[] = {stopMtr, startMtr, ddZnSet, setMtrTrgt};
+
+//UART reciever driver. The UART recieves and concats each char into received_chars untill the EOl is received\n it then extracts the command id and the value sent with it if there is one and calls the command functiom
+void serialControl()
+{
   while (Serial.available())
   {
     char inChar = (char)Serial.read();
     received_chars.concat(inChar);
     if (inChar == '\n')
     {
-	  char command = received_chars[0];
-	  received_chars.remove(0,1);
-      float numInt = received_chars.toInt();
-      float numFlt = received_chars.toFloat();
-	  Serial.print("Command == ");
-	  Serial.println(command);
-	  Serial.print("Num int == ");
-	  Serial.println(numInt);
-	  Serial.print("Num flt == ");
-	  Serial.println(numFlt);
-      if(command == 's'){//stop
-        radPerSec = 0;
-        mxRads = 0;
-        received_chars = "";
-        Serial.println("Stopping Motor.");
-        break;
-      }
-      else if(command == 'g'){//go
-        radPerSec = 0;
-        mxRads = 300;
-        received_chars = "";
-        Serial.println("Starting Motor.");
-        break;
-      }
-      else if(command == 'd'){//go
-	  	driver.dead_zone = numFlt;
-		radPerSec = 0;
-        received_chars = "";
-        Serial.print("Setting dead zone to ");
-        Serial.println(numFlt);
-        break;
-      }
-	  else if(command == 't'){
-		if(mxRads < numInt){
-			opMode = INCREMENT;
-		}
-		else{
-			opMode = DECREMENT;
-		}
-		mxRads = numInt;
-		spinIt = 1;
-
-		Serial.print("Changing target speed to ");
-		Serial.print(numInt*9.549297);
-		Serial.println("RPM.");
-        received_chars = "";
-		break;
-	  }
-      else{
-        target = numInt;
-        Serial.print("PID.P = ");
-        Serial.println(target);
-        received_chars = "";
-      }    
+      String command = (String)received_chars[0];
+      int cmdInt = command.toInt();
+      received_chars.remove(0, 1);
+      numInt = received_chars.toInt();
+      numFlt = received_chars.toFloat();
+      uartLUT[cmdInt]();
+      received_chars = "";
     }
   }
 }
 
+
 void setup()
 {
-  Serial.begin(115200);
-  SimpleFOCDebug::enable();
+  //UART set to a baud of 4800
+  Serial.begin(4800);
 
-  //initializes the psu and driver
+  // initializes the psu and driver
   driver.voltage_power_supply = 20;
-  driver.pwm_frequency = 20000;
-  //driver.dead_zone = 0.02;
+  driver.pwm_frequency = 50000;
+  // driver.dead_zone = 0.02;
   driver.init();
 
   motor.linkDriver(&driver);
-  currentsense.linkDriver(&driver);  
+  currentsense.linkDriver(&driver);
   motor.voltage_limit = 20;
-  //motor.current_limit = 1;
+  // motor.current_limit = 1;
   motor.velocity_limit = 2212;
-  
 
-  //Sets up openloop FOC
+  // Sets up openloop FOC
   motor.controller = MotionControlType::velocity_openloop;
   // set FOC modulation type to sinusoidal
-  motor.foc_modulation = FOCModulationType::SinePWM;
-
+  motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
 
   motor.init();
   currentsense.init();
   motor.linkCurrentSense(&currentsense);
-
+  currentsense.driverAlign(1);
 
   motor.initFOC();
 
-  
   delay(1000);
-
 }
-
 
 void loop()
 {
-  //starts the motor moving at radPerSec
+  // starts the motor moving at radPerSec
+  motor.monitor();
   motor.loopFOC();
   motor.move(radPerSec);
-  //this loop controls the rate at which the motor speeds up
-  switch(opMode){
-	case INCREMENT:
-		if (spinIt%spinItSpeed == 0 && radPerSec<mxRads){
-			radPerSec++;
-			spinIt = 1;
-		}
-		else{
-			spinIt++;
-		}
-		break;
-	case DECREMENT:
-		if (spinIt%spinItSpeed == 0 && radPerSec>=mxRads){
-			radPerSec--;
-			spinIt = 1;
-		}
-		else{
-			spinIt++;
-		}
-		break;
-  }  
+  // this loop controls the rate at which the motor speeds up
+  switch (opMode)
+  {
+  case INCREMENT:
+    if (spinIt % spinItSpeed == 0 && radPerSec < mxRads)
+    {
+      radPerSec++;
+      spinIt = 1;
+    }
+    else
+    {
+      spinIt++;
+    }
+    break;
+  case DECREMENT:
+    if (spinIt % spinItSpeed == 0 && radPerSec >= mxRads)
+    {
+      radPerSec--;
+      spinIt = 1;
+    }
+    else
+    {
+      spinIt++;
+    }
+    break;
+  }
+
   serialControl();
 }
